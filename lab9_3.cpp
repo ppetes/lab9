8dbf9b1e295d0500a6591de57e2abe73d680c3de
ratchapon prangthong <ppete2547@gmail.com>
2022-12-23 15:36:50 +0700

    lab9_3 fish

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,45 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int age,high,bou;
+cout << "Enter your age: ";
+cin >> age;
+    if(age <= 25){
+   cout << "Enter your height: ";
+    cin >> high;
+    if(high < 100){
+    cout << "Your character = Chopper";
+    }
+    else if(high < 180){
+     cout << "Your character = Usopp";
+     }
+     else{
+    cout << "Enter your bounty: ";
+    cin >> bou;
+        if(bou > 1100000000){
+        cout << "Your character = Zoro";
+        }
+        else{
+        cout << "Your character = Sanji";
+        }
+    }
+    }
+    else if(age <= 60){
+    cout << "Enter your bounty: ";
+    cin >> bou;
+        if(bou > 500000000){
+            cout << "Your character = Jinbe";
+        }
+        else{
+        cout << "Your character = Franky";
+        }
+        }
+        else{
+        cout << "Your character = Brook";
+        }
+        }
+
+        
+
